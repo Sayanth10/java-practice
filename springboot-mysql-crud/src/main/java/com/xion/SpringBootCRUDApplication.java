d8e066338632a1b1e9b14ff525020a0d9a469a69
Sayanthan Mahendran <sayanthanmahendran@Sayanthans-MacBook-Pro.local>
2024-03-13 21:46:57 +0800

    Initiated project for spring boot CRUD example

---
+++ springboot-mysql-crud/src/main/java/com/xion/SpringBootCRUDApplication.java
@@ -0,0 +1,13 @@
+package com.xion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCRUDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCRUDApplication.class);
+    }
+
+}
